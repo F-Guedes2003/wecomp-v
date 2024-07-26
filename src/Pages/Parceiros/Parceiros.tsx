f7ff128b1017d8c7bc7c10c3658d13984d4fe86b
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-07-26 15:55:38 -0300

    adjusted the background size of partners

--- src/Pages/Parceiros/Parceiros.tsx
+++ src/Pages/Parceiros/Parceiros.tsx
@@ -34,7 +34,7 @@ export default function Parceiros({ isLandscape }: IParceirosProps) {
     fetchPartners();
   }, []);
 
-  const width = useBreakpointValue({ base: "75vw", md: "70vw", lg: "75vw" }) ?? "80vw";
+  const width = useBreakpointValue({ base: "75vw", md: "70vw", lg: "130vh" }) ?? "80vw";
 
   return (
     <div id="parceiros" className={`partners ${isLandscape ? "partners-landscape" : ""}`}>
